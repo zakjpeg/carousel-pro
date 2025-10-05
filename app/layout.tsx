176ca18d33373203aa7aeb0bc43497048d691e18
zakjpeg <elassaadzak@gmail.com>
2025-10-04 20:04:11 -0700

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter_Tight } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const interTight = Inter_Tight({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter-tight', // optional if you want to use CSS variable
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-dark antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,105 @@
+'use client';
 import Image from "next/image";
+import { RiVerifiedBadgeFill } from "react-icons/ri";
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const str = "Thanks for using Carousel Pro! I hope this tool is fun and easy to use.\n\nFor any questions or concerns, please reach out to @zakjpeg on twitter."
+  const [name, setName] = useState<string>("Athletes & Assets");
+  const [handle, setHandle] = useState<string>("athletesandassets");
+  const [league, setLeague] = useState<string>("News");
+  const [content, setContent] = useState<string>(str);
+  const [showLeague, setShowLeage] = useState<boolean>(true);
+  const [verified, setVerified] = useState<boolean>(true);
+  const [profilePic, setProfilePic] = useState<string>('defaultpfp.png');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleImgInput = (e: Blob) => {
+    var reader = new FileReader();
+    reader.readAsDataURL(e);
+  }
+
+
+  return (
+    <div className="m-15 flex flex-row justify-center gap-10">
+      {/* Left Menu */}
+      <div className="flex flex-col p-8 h-120 w-2/6 bg-medium rounded-2xl shadow-s">
+        <div className="font-bold mb-20 text-xl">Carousel Pro
+          <p className="font-normal text-sm">Export carousel elements that look like tweets.</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <input type="file" accept="image/*"
+        onChange={(e) => {handleImgInput(e)}}
+        />
+        {/* Button Row*/}
+        <div className="flex flex-row justify-end items-center gap-3 mb-6 text-sm">
+          <button className={clsx(
+            "px-2 rounded-lg cursor-pointer transition duration-250 eas-in-out",
+            verified ? "bg-white text-dark shadow-m" : "bg-medium text-text-muted shadow-m"
+          )} 
+          onClick={() => {setVerified(!verified)}}>
+            Verified
+          </button>
+          <button className={clsx(
+            "px-2 rounded-lg cursor-pointer transition duration-250 eas-in-out",
+            showLeague ? "bg-white text-dark shadow-m" : "bg-medium text-text-muted shadow-m"
+          )} 
+          onClick={() => {setShowLeage(!showLeague)}}>
+            League
+          </button>
+
+        </div>
+        <input type="text" placeholder="Name" className="bg-light rounded-4xl px-3 py-1 shadow-s text-muted w-full"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}/>
+        <div className="flex flex-row justify-between gap-2 my-3">
+          <input type="text" placeholder="Handle" className="bg-light rounded-4xl px-3 py-1 shadow-s text-muted w-full"
+          onChange={(e) => {
+            setHandle(e.target.value);
+          }}/>
+          <input type="text" placeholder="League" className="bg-light rounded-4xl px-3 py-1 shadow-s text-muted w-full"
+          disabled={!showLeague}
+          onChange={(e) => {
+            setLeague(e.target.value);
+          }}/>
+        </div>
+        <textarea name="" id="" placeholder="Content" className="bg-light rounded-2xl py-1 px-3 shadow-s min-h-20 overflow-y-scroll"
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}/>
+      </div>
+
+      {/* Right Menu */}
+      <div className="flex flex-col justify-between gap-10">
+        {/* Tweet Dark*/}
+        <div className="flex flex-col p-8 h-min w-[400px] bg-medium rounded-2xl shadow-s">
+
+          {/* Top Row */}
+          <div className="flex flex-row justify-between mb-3">
+            {/* Left Side */}
+            <div className="flex flex-row">
+              <img src={profilePic} className="w-[50px] rounded-full mr-3 border-2 border-brand-orange"/>
+              {/* Name and Handle */}
+              <div className="flex flex-col">
+                <p className="font-semibold text-lg flex flex-row items-center gap-2">{name}{verified && <RiVerifiedBadgeFill color="1DA1F2"/>}
+                </p>
+                <p className="font-regular text-sm text-text-muted">@{handle}</p>
+              </div>
+            </div>
+            {/* Right Side */}
+            { showLeague &&
+            <div className="bg-brand-orange font-semibold h-min w-min px-1.5 py-0.5 rounded-lg text-sm">
+              {league}
+            </div>
+            }
+          </div>
+
+          {/* Bottom Row */}
+          <p className="whitespace-pre-wrap">{content}</p>
+
+        </div>
+
+      </div>
     </div>
   );
 }
