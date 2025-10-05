02c8a8c90d5d69401bc99c56d012ce79c9a96049
zakjpeg <elassaadzak@gmail.com>
2025-10-05 01:21:20 -0700

    feat(): added preview

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,9 @@ export default function Home() {
   const [showLeague, setShowLeage] = useState<boolean>(true);
   const [verified, setVerified] = useState<boolean>(true);
   const [profilePic, setProfilePic] = useState<string>('defaultpfp.png');
+  const [background, setBackground] = useState<string>('https://www.bigfacebrand.com/cdn/shop/files/HP-HERO-MOB-BIGFACE-GP_722x.jpg?v=1755550700');
 
-  const handleImgInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePfpInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
 
@@ -27,15 +28,15 @@ export default function Home() {
 
 
   return (
-    <div className="m-15 flex flex-col md:flex-row items-center md:items-start justify-center gap-10">
+    <div className="m-15 flex flex-col lg:flex-row items-center lg:items-start justify-center gap-10">
       {/* Left Menu */}
-      <div className="flex flex-col p-8 h-120 md:w-2/6 bg-medium rounded-2xl shadow-s">
-        <div className="font-bold mb-20 text-xl">Carousel Pro
+      <div className="flex flex-col p-8 h-[570px] lg:w-2/6 bg-medium rounded-2xl shadow-s">
+        <div className="font-bold mb-20 text-xl">A&A Carousel Pro
           <p className="font-normal text-sm text-text-muted">Export carousel elements that look like tweets.</p>
         </div>
         <input type="file" accept="image/*"
         className=""
-        onChange={(e) => {handleImgInput(e)}}
+        onChange={(e) => {handlePfpInput(e)}}
         />
         {/* Button Row*/}
         <div className="flex flex-row justify-end items-center gap-3 mb-6 text-sm">
@@ -76,10 +77,53 @@ export default function Home() {
           }}/>
       </div>
 
+      {/* Composite */}
+      <div className="flex flex-col justify-between gap-10">
+        <p className="self-center font-semibold text-lg">Preview</p>
+        <div className="flex relative rounded-3xl overflow-hidden">
+          {/* Tweet Light*/}
+          <div className="flex flex-col p-5 h-min w-[400px] scale-85 absolute bottom-4 bg-white rounded-2xl shadow-s z-10">
+
+            {/* Top Row */}
+            <div className="flex flex-row justify-between mb-3">
+              {/* Left Side */}
+              <div className="flex flex-row">
+                <img src={profilePic} className="w-[50px] h-[50px] rounded-full mr-3 border-2 border-brand-orange object-cover"/>
+                {/* Name and Handle */}
+                <div className="flex flex-col">
+                  <p className="font-semibold text-black text-lg flex flex-row items-center gap-2">{name}{verified && <RiVerifiedBadgeFill color="1DA1F2"/>}
+                  </p>
+                  <p className="font-regular text-sm text-gray-400">@{handle}</p>
+                </div>
+              </div>
+              {/* Right Side */}
+              { showLeague &&
+              <div className="bg-brand-orange font-semibold h-min w-min px-1.5 py-0.5 rounded-lg text-sm">
+                {league}
+              </div>
+              }
+            </div>
+
+            {/* Bottom Row */}
+            <p className="whitespace-pre-wrap text-gray-900">{content}</p>
+
+          </div>
+          {/* Gradient Overlay */}
+          <div className="w-full h-1/2 absolute bottom-0 bg-gradient-to-t from-brand-deep-navy to-brand-orange/0"></div>
+          {/* Gradient Overlay */}
+          <div className="w-full h-1/5 absolute top-0 bg-gradient-to-b from-black/55 to-black/0"></div>
+
+          {/* Background Image */}
+          <img src={background} alt="" className="w-[400px] h-[500px] object-cover"/>
+        </div>        
+      </div>
+
+
       {/* Right Menu */}
       <div className="flex flex-col justify-between gap-10">
+        <p className="self-center font-semibold text-lg">Theme</p>
         {/* Tweet Dark*/}
-        <div className="flex flex-col p-8 h-min w-[400px] bg-medium rounded-2xl shadow-s">
+        <div className="flex flex-col p-5 h-min w-[400px] bg-medium rounded-2xl shadow-s interactive-card">
 
           {/* Top Row */}
           <div className="flex flex-row justify-between mb-3">
@@ -105,9 +149,35 @@ export default function Home() {
           <p className="whitespace-pre-wrap">{content}</p>
 
         </div>
+        {/* Tweet Dark*/}
+        <div className="flex flex-col p-5 h-min w-[400px] bg-brand-deep-navy rounded-2xl shadow-s interactive-card">
 
+          {/* Top Row */}
+          <div className="flex flex-row justify-between mb-3">
+            {/* Left Side */}
+            <div className="flex flex-row">
+              <img src={profilePic} className="w-[50px] h-[50px] rounded-full mr-3 border-2 border-brand-orange object-cover"/>
+              {/* Name and Handle */}
+              <div className="flex flex-col">
+                <p className="font-semibold text-lg flex flex-row items-center gap-2">{name}{verified && <RiVerifiedBadgeFill color="1DA1F2"/>}
+                </p>
+                <p className="font-regular text-sm text-text-muted">@{handle}</p>
+              </div>
+            </div>
+            {/* Right Side */}
+            { showLeague &&
+            <div className="bg-brand-orange font-semibold h-min w-min px-1.5 py-0.5 rounded-lg text-sm">
+              {league}
+            </div>
+            }
+          </div>
+
+          {/* Bottom Row */}
+          <p className="whitespace-pre-wrap">{content}</p>
+
+        </div>
         {/* Tweet Light*/}
-        <div className="flex flex-col p-8 h-min w-[400px] bg-white rounded-2xl shadow-s">
+        <div className="flex flex-col p-5 h-min w-[400px] bg-white rounded-2xl shadow-white-s interactive-card">
 
           {/* Top Row */}
           <div className="flex flex-row justify-between mb-3">
@@ -133,8 +203,8 @@ export default function Home() {
           <p className="whitespace-pre-wrap text-gray-900">{content}</p>
 
         </div>
-
       </div>
+
     </div>
   );
 }
