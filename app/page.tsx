7ff84928b4bcfd09311c2d5aefbc4dea177549e6
zakjpeg <elassaadzak@gmail.com>
2025-10-05 01:44:47 -0700

    made stuff more customizable

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import clsx from 'clsx';
 
 export default function Home() {
-  const str = "Thanks for using Carousel Pro! I hope this tool is fun and easy to use.\n\nFor any questions or concerns, please reach out to @zakjpeg on twitter."
+  const str = "Thanks for using A&A Carousel Pro! I hope this tool is fun and easy to use.\n\nFor any questions or concerns, please reach out to Zak via Slack."
   const [name, setName] = useState<string>("Athletes & Assets");
   const [handle, setHandle] = useState<string>("athletesandassets");
   const [league, setLeague] = useState<string>("News");
@@ -14,6 +14,7 @@ export default function Home() {
   const [verified, setVerified] = useState<boolean>(true);
   const [profilePic, setProfilePic] = useState<string>('defaultpfp.png');
   const [background, setBackground] = useState<string>('https://www.bigfacebrand.com/cdn/shop/files/HP-HERO-MOB-BIGFACE-GP_722x.jpg?v=1755550700');
+  const [theme, setTheme] = useState<number>(2);
 
   const handlePfpInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -26,6 +27,88 @@ export default function Home() {
     reader.readAsDataURL(file);
   }
 
+  const handleBgInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    var reader = new FileReader();
+    reader.onload = () => {
+      setBackground(reader.result as string);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  const handleThemeChange = (code: number) => {
+    if (!Number.isNaN(code)) {
+      setTheme(code);
+    }
+  }
+
+  // Function: tweetContents
+  // Purpose: Returns Tweet contents from states
+  // Input: Number. 0 means dark mode, 1 means light mode
+  // Output: HTML 
+  //
+  const tweetContents = (num: number) => {
+    if (num == 0) {
+      return (
+          <div>
+
+            {/* Top Row */}
+            <div className="flex flex-row justify-between mb-3">
+              {/* Left Side */}
+              <div className="flex flex-row">
+                <img src={profilePic} className="w-[50px] h-[50px] rounded-full mr-3 border-2 border-brand-orange object-cover"/>
+                {/* Name and Handle */}
+                <div className="flex flex-col">
+                  <p className="font-semibold text-lg flex flex-row items-center gap-2">{name}{verified && <RiVerifiedBadgeFill color="1DA1F2"/>}
+                  </p>
+                  <p className="font-regular text-sm text-text-muted">@{handle}</p>
+                </div>
+              </div>
+              {/* Right Side */}
+              { showLeague &&
+              <div className="bg-brand-orange font-semibold h-min w-min px-1.5 py-0.5 rounded-lg text-sm whitespace-nowrap">
+                {league}
+              </div>
+              }
+            </div>
+
+            {/* Bottom Row */}
+            <p className="whitespace-pre-wrap">{content}</p>
+
+          </div>
+      );
+    } else if (num == 1) {
+      return (
+        <div>
+          {/* Top Row */}
+          <div className="flex flex-row justify-between mb-3">
+            {/* Left Side */}
+            <div className="flex flex-row">
+              <img src={profilePic} className="w-[50px] h-[50px] rounded-full mr-3 border-2 border-brand-orange object-cover"/>
+              {/* Name and Handle */}
+              <div className="flex flex-col">
+                <p className="font-semibold text-black text-lg flex flex-row items-center gap-2">{name}{verified && <RiVerifiedBadgeFill color="1DA1F2"/>}
+                </p>
+                <p className="font-regular text-sm text-gray-400">@{handle}</p>
+              </div>
+            </div>
+            {/* Right Side */}
+            { showLeague &&
+            <div className="bg-brand-orange font-semibold h-min w-min px-1.5 py-0.5 rounded-lg text-sm whitespace-nowrap">
+              {league}
+            </div>
+            }
+          </div>
+
+          {/* Bottom Row */}
+          <p className="whitespace-pre-wrap text-gray-900">{content}</p>
+        </div>
+      );
+    }
+  }
+  
 
   return (
     <div className="m-15 flex flex-col lg:flex-row items-center lg:items-start justify-center gap-10">
@@ -37,6 +120,10 @@ export default function Home() {
         <input type="file" accept="image/*"
         className=""
         onChange={(e) => {handlePfpInput(e)}}
+        />
+        <input type="file" accept="image/*"
+        className=""
+        onChange={(e) => {handleBgInput(e)}}
         />
         {/* Button Row*/}
         <div className="flex flex-row justify-end items-center gap-3 mb-6 text-sm">
@@ -81,33 +168,24 @@ export default function Home() {
       <div className="flex flex-col justify-between gap-10">
         <p className="self-center font-semibold text-lg">Preview</p>
         <div className="flex relative rounded-3xl overflow-hidden">
-          {/* Tweet Light*/}
-          <div className="flex flex-col p-5 h-min w-[400px] scale-85 absolute bottom-4 bg-white rounded-2xl shadow-s z-10">
-
-            {/* Top Row */}
-            <div className="flex flex-row justify-between mb-3">
-              {/* Left Side */}
-              <div className="flex flex-row">
-                <img src={profilePic} className="w-[50px] h-[50px] rounded-full mr-3 border-2 border-brand-orange object-cover"/>
-                {/* Name and Handle */}
-                <div className="flex flex-col">
-                  <p className="font-semibold text-black text-lg flex flex-row items-center gap-2">{name}{verified && <RiVerifiedBadgeFill color="1DA1F2"/>}
-                  </p>
-                  <p className="font-regular text-sm text-gray-400">@{handle}</p>
-                </div>
-              </div>
-              {/* Right Side */}
-              { showLeague &&
-              <div className="bg-brand-orange font-semibold h-min w-min px-1.5 py-0.5 rounded-lg text-sm">
-                {league}
-              </div>
-              }
-            </div>
-
-            {/* Bottom Row */}
-            <p className="whitespace-pre-wrap text-gray-900">{content}</p>
-
+          {/* Tweet Dark*/}
+          {theme == 0 &&
+          <div className="flex flex-col p-5 h-min w-[400px] scale-85 absolute bottom-4 bg-medium rounded-2xl shadow-s z-10">
+            {tweetContents(0)}
           </div>
+          }
+          {/* Tweet Navy*/}
+          {theme == 1 &&
+          <div className="flex flex-col p-5 h-min w-[400px] scale-85 absolute bottom-4 bg-brand-deep-navy rounded-2xl shadow-s z-10">
+            {tweetContents(0)}
+          </div>
+          }
+          {/* Tweet Light*/}
+          {theme == 2 && 
+          <div className="flex flex-col p-5 h-min w-[400px] scale-85 absolute bottom-4 bg-white rounded-2xl shadow-s z-10">
+            {tweetContents(1)}
+          </div>
+          }
           {/* Gradient Overlay */}
           <div className="w-full h-1/2 absolute bottom-0 bg-gradient-to-t from-brand-deep-navy to-brand-orange/0"></div>
           {/* Gradient Overlay */}
@@ -123,85 +201,28 @@ export default function Home() {
       <div className="flex flex-col justify-between gap-10">
         <p className="self-center font-semibold text-lg">Theme</p>
         {/* Tweet Dark*/}
-        <div className="flex flex-col p-5 h-min w-[400px] bg-medium rounded-2xl shadow-s interactive-card">
-
-          {/* Top Row */}
-          <div className="flex flex-row justify-between mb-3">
-            {/* Left Side */}
-            <div className="flex flex-row">
-              <img src={profilePic} className="w-[50px] h-[50px] rounded-full mr-3 border-2 border-brand-orange object-cover"/>
-              {/* Name and Handle */}
-              <div className="flex flex-col">
-                <p className="font-semibold text-lg flex flex-row items-center gap-2">{name}{verified && <RiVerifiedBadgeFill color="1DA1F2"/>}
-                </p>
-                <p className="font-regular text-sm text-text-muted">@{handle}</p>
-              </div>
-            </div>
-            {/* Right Side */}
-            { showLeague &&
-            <div className="bg-brand-orange font-semibold h-min w-min px-1.5 py-0.5 rounded-lg text-sm">
-              {league}
-            </div>
-            }
-          </div>
-
-          {/* Bottom Row */}
-          <p className="whitespace-pre-wrap">{content}</p>
-
+        <div className="flex flex-col p-5 h-min w-[400px] bg-medium rounded-2xl shadow-s interactive-card"
+        onClick={() => {
+          handleThemeChange(0);
+        }}
+        >
+          {tweetContents(0)}
         </div>
-        {/* Tweet Dark*/}
-        <div className="flex flex-col p-5 h-min w-[400px] bg-brand-deep-navy rounded-2xl shadow-s interactive-card">
-
-          {/* Top Row */}
-          <div className="flex flex-row justify-between mb-3">
-            {/* Left Side */}
-            <div className="flex flex-row">
-              <img src={profilePic} className="w-[50px] h-[50px] rounded-full mr-3 border-2 border-brand-orange object-cover"/>
-              {/* Name and Handle */}
-              <div className="flex flex-col">
-                <p className="font-semibold text-lg flex flex-row items-center gap-2">{name}{verified && <RiVerifiedBadgeFill color="1DA1F2"/>}
-                </p>
-                <p className="font-regular text-sm text-text-muted">@{handle}</p>
-              </div>
-            </div>
-            {/* Right Side */}
-            { showLeague &&
-            <div className="bg-brand-orange font-semibold h-min w-min px-1.5 py-0.5 rounded-lg text-sm">
-              {league}
-            </div>
-            }
-          </div>
-
-          {/* Bottom Row */}
-          <p className="whitespace-pre-wrap">{content}</p>
-
+        {/* Tweet Navy*/}
+        <div className="flex flex-col p-5 h-min w-[400px] bg-brand-deep-navy rounded-2xl shadow-s interactive-card"
+        onClick={() => {
+          handleThemeChange(1);
+        }}
+        >
+          {tweetContents(0)}
         </div>
         {/* Tweet Light*/}
-        <div className="flex flex-col p-5 h-min w-[400px] bg-white rounded-2xl shadow-white-s interactive-card">
-
-          {/* Top Row */}
-          <div className="flex flex-row justify-between mb-3">
-            {/* Left Side */}
-            <div className="flex flex-row">
-              <img src={profilePic} className="w-[50px] h-[50px] rounded-full mr-3 border-2 border-brand-orange object-cover"/>
-              {/* Name and Handle */}
-              <div className="flex flex-col">
-                <p className="font-semibold text-black text-lg flex flex-row items-center gap-2">{name}{verified && <RiVerifiedBadgeFill color="1DA1F2"/>}
-                </p>
-                <p className="font-regular text-sm text-gray-400">@{handle}</p>
-              </div>
-            </div>
-            {/* Right Side */}
-            { showLeague &&
-            <div className="bg-brand-orange font-semibold h-min w-min px-1.5 py-0.5 rounded-lg text-sm">
-              {league}
-            </div>
-            }
-          </div>
-
-          {/* Bottom Row */}
-          <p className="whitespace-pre-wrap text-gray-900">{content}</p>
-
+        <div className="flex flex-col p-5 h-min w-[400px] bg-white rounded-2xl shadow-white-s interactive-card"
+        onClick={() => {
+          handleThemeChange(2);
+        }}
+        >
+          {tweetContents(1)}
         </div>
       </div>
 
